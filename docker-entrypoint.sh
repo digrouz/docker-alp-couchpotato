dd24b7f4339f9491c55fbb2b5157738eb66d5834
DI GREGORIO Nicolas <nicolas.digregorio@gmail.com>
2016-10-04 17:27:05 +0000

    added initial docker-entrypoint.sh

---
+++ docker-entrypoint.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+local MYUSER="couchpotato"
+local MYGID="10009"
+local MYUID="10009"
+
+ConfigureUser () {
+  # Managing user
+  if [ -n "${DOCKUID}" ]; then
+    MYUID="${DOCKUID}"
+  fi
+  # Managing group
+  if [ -n "${DOCKGID}" ]; then
+    MYGID="${DOCKGID}"
+  fi
+  local OLDHOME
+  local OLDGID
+  local OLDUID
+  /bin/grep -q "${MYUSER}" /etc/passwd
+  if [ $? -eq 0 ]; then
+    OLDUID=$(/usr/bin/id -u "${MYUSER}")
+    OLDGID=$(/usr/bin/id -g "${MYUSER}")
+    if [ "${DOCKUID}" != "${OLDUID}" ]; then
+      OLDHOME=$(/bin/echo "~${MYUSER}")
+      /usr/sbin/deluser "${MYUSER}"
+      /usr/bin/logger "Deleted user ${MYUSER}"
+    fi
+    /bin/grep -q "${MYUSER}" /etc/group
+    if [ $? -eq 0 ]; then
+      local OLDGID=$(/usr/bin/id -g "${MYUSER}")
+      if [ "${DOCKGID}" != "${OLDGID}" ]; then
+        /usr/sbin/delgroup "${MYUSER}"
+        /usr/bin/logger "Deleted group ${MYUSER}"
+      fi
+    fi
+  fi
+  /usr/sbin/addgroup -S -g "${MYGID}" "${MYUSER}"
+  /usr/sbin/adduser -S -D -H -s /sbin/nologin -G "${MYUSER}" -h "${OLDHOME}" -u "${MYUID}" "${MYUSER}"
+  if [ -n "${OLDUID}" ] && [ "${DOCKUID}" != "${OLDUID}" ]; then
+    /usr/bin/find / -user "${OLDUID}" -exec /bin/chown ${MYUSER} {} \;
+  fi
+  if [ -n "${OLDGID}" ] && [ "${DOCKGID}" != "${OLDGID}" ]; then
+    /usr/bin/find / -group "${OLDGID}" -exec /bin/chgrp ${MYUSER} {} \;
+  fi
+}
+
+ConfigureUser
+
+if [ "$1" = 'couchpotato' ]; then
+    /bin/chown -R "${MYUSER}":"${MYUSER}" /config /opt/couchpotato
+    /bin/chmod -R g+w /config /opt/couchpotato
+    exec /sbin/su-exec "${MYUSER}" /usr/bin/python /opt/couchpotato/CouchPotato.py --nolaunch --datadir=/config/ --config=/config/config.ini 
+fi
+
+exec "$@"
